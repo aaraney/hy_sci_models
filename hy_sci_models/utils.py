381fe0ccbcbe9079b53fd5f14c0180ed3a2332d8
Austin Raney <aaraney@crimson.ua.edu>
2020-11-20 13:41:07 -0500

    add highest_correlation_after_transformation method for computing the highest pearson correlation between a set of features and a label from a provided function mapping

--- hy_sci_models/utils.py
+++ hy_sci_models/utils.py
@@ -54,3 +54,66 @@ def plot_train_val_loss(training_loss, validation_loss):
     ax.set_xlabel("Epochs")
     ax.set_ylabel("Learning Rate")
     ax.legend()
+
+
+def highest_correlation_after_transformation(
+    features: pd.DataFrame, label: pd.Series, transformation_mapping: dict
+):
+    """Determine highest correlation coeficient between features and some label
+    from a dictionary of passed in transformation functions.
+
+    Example:
+        transformation_map = {
+            "inverse": lambda x: 1 /x,
+            "log": lambda x: np.log(x),
+            "cube_root": lambda x: x ** (1/3)
+        }
+
+        highest_correlation_after_transformation(df[['feature_1', 'feature_2', 'feature_3']], df['label'], transformation_map)
+
+    """
+    # Empty dataframe
+    correlation_df_all_applied_transforms = pd.DataFrame()
+
+    for transformation_key_name, transformation in transformation_mapping.items():
+        # Apply each transform to the features then,
+        # get the pearsons correlation with the label.
+        # Cast the pd.Series to a df and transpose so
+        # feature names are column names
+        corr_df = pd.DataFrame(features.apply(transformation).corrwith(label)).T
+
+        # Set a col, transformation to the name of the
+        # applied transformation, as per the dictionary key
+        corr_df["transformation"] = transformation_key_name
+
+        # Concat the resulting dataframe to the empty dataframe
+        correlation_df_all_applied_transforms = pd.concat(
+            [correlation_df_all_applied_transforms, corr_df]
+        )
+
+    # reset the index, droping erroneous prior index
+    correlation_df_all_applied_transforms = (
+        correlation_df_all_applied_transforms.reset_index(drop=True)
+    )
+
+    # make longer. feature col is name of col from input df
+    # r is the pearson correlation with the label
+    correlation_df_all_long = correlation_df_all_applied_transforms.melt(
+        id_vars=["transformation"], value_name="r", var_name="feature"
+    )
+
+    # Square to get rid of sign differences and relate correlation magnitudes
+    correlation_df_all_long["sq_value"] = correlation_df_all_long["r"] ** 2
+
+    result_df = (
+        correlation_df_all_long.groupby("feature")
+        # Get rows with the highest R**2, or put differently, have the highest magnitude r value
+        .apply(lambda ds: ds[ds["sq_value"] == ds["sq_value"].max()])[
+            ["feature", "transformation", "r"]
+        ]
+    )
+    # Sort columns by feature name
+    result_df = result_df.iloc[result_df["feature"].str.lower().argsort()].reset_index(
+        drop=True
+    )
+    return result_df
