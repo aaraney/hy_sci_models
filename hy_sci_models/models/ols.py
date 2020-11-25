89fb9298294a6e611662b7624a8845584c9c07c0
Austin Raney <aaraney@crimson.ua.edu>
2020-11-25 17:55:21 -0500

    implement ols model output dataclass

--- hy_sci_models/models/ols.py
+++ hy_sci_models/models/ols.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
+from dataclasses import dataclass
+from .abstractmodeloutput import AbstractModelOutput
+
 
 def OLS(y, X):
     return sm.OLS(y, X)
@@ -15,3 +18,11 @@ def train(model):
 
 def test(model, X):
     return model.predict(X)
+
+
+@dataclass
+class OLSModelOutput(AbstractModelOutput):
+    X_train: np.array
+    X_test: np.array
+    y_train: np.array
+    y_test: np.array
